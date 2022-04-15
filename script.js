d4994716ea2d0a9c623b771d65debbd2cca18a88
MatthewT12 <matttyndell@gmail.com>
2022-04-15 13:08:46 -0400

    worked on the javascript and html with some css

---
+++ script.js
@@ -0,0 +1,98 @@
+var questions = [
+    {
+        title: "commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+    },
+    {
+        title: "commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
+    }
+];
+
+var questionsEl = document.getElementById("questons");
+var timerEl = document.getElementById("time");
+var choicesEl = document.getElementById("choices");
+var submitBtn = document.getElementById("submit");
+var startBtn = document.getElementById("start");
+var initialsEl = document.getElementById("initials");
+var feedbackEl = document.getElementById("feedback");
+
+var currentQuestionIndex = 0;
+var time = questions.length * 15;
+var timerId;
+
+
+function startQuiz() {
+    timerId = setInterval(clockTick, 1000);
+    timerEl.textContent = time;
+    getQuestion();
+}
+
+function clockTick() {
+    time--;
+    timerEl.textContent = time;
+    if(time <= 0) {
+        quizEnd();
+    }
+
+    
+}
+
+function quizEnd() {
+    clearInterval(timerId);
+    var endScreenEl = document.getElementById("end-screen");
+    endScreenEl.removeAttribute("class");
+    var finalScoreEl = document.getElementById("final-score");
+    finalScoreEl.textContent = time;
+    questionsEl.setAttribute("class", "hide");
+}
+
+function getQuestion() {
+    var currentQuestion = questions[currentQuestionIndex];
+    var titleEl = document.getElementById("question-title");
+    titleEl.textContent = currentQuestion.title;
+    choicesEl.innerHTML = "";
+
+    currentQuestion.choices.forEach(function(choice, i) {
+        var choiceNode = document.createElement("button");
+        choiceNode.setAttribute("class", "choice");
+        choiceNode.setAttribute("value", choice);
+
+        choiceNode.textContent = i + choice;
+        choiceNode.onclick = questionClick;
+        choicesEl.appendChild(choiceNode);
+
+    })
+
+}
+
+function questionClick() {
+    if(this.value !== questions[currentQuestionIndex].answer) {
+        time -= 10;
+
+        if (time < 0) {
+            time = 0;
+        }
+        timerEl.textContent = time;
+        feedbackEl.textContent = "Wrong!" 
+    } else {
+        feedbackEl.textContent = "Correct";
+    }
+    feedbackEl.setAttribute("class", "feedback")
+    setTimeout(function() {
+        feedbackEl.setAttribute("class", "feedback hide");
+    }, 1000);
+
+    currentQuestionIndex++;
+
+    if(currentQuestionIndex === questions.length){
+        quizEnd();
+    }else {
+        getQuestion();
+    }
+
+}
+
+startBtn.onclick = startQuiz;
